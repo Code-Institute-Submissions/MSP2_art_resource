1ba1cff2ba6c1af3ee01d64f1400edcb991bfd08
Mike Green <m.d.j.green@ntlworld.com>
2020-09-12 10:09:43 +0000

    Initial file structure set up

---
+++ assets/js/metart.js
@@ -0,0 +1,3 @@
+// Setting up api portal
+const apiMet = "https://collectionapi.metmuseum.org/public/collection/v1/";
+const apiMetObject = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
