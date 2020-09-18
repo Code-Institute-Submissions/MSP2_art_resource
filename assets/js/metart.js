05f99b3f17b7c3b9976e21021a2df88125b993ff
Mike Green <m.d.j.green@ntlworld.com>
2020-09-18 10:23:10 +0000

    Script to access and display API department

--- assets/js/metart.js
+++ assets/js/metart.js
@@ -1,3 +1,32 @@
 // Setting up api portal
 const apiMet = "https://collectionapi.metmuseum.org/public/collection/v1/";
 const apiMetObject = "https://collectionapi.metmuseum.org/public/collection/v1/objects/";
+
+
+/*
+        Scripts to access Met API for their art department details
+*/
+
+function getMetDept(cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET",apiMet + "departments");
+    xhr.send();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {  
+            cb(JSON.parse(this.responseText));
+            console.log("********  JSON response text "+JSON.parse(this.responseText));
+        } else {
+            console.log("******** state "+ this.readyState +" ******* status " +this.status);
+        };
+    };
+};
+
+function writeDepts() {
+    getMetDept(function(item) {
+       var depts = [];
+       depts=item.departments;
+       depts.forEach(function(item) {
+                document.getElementById("metArtDept").innerHTML += item.departmentId+") "+item.displayName+" <br>";
+            });
+    });
+};
